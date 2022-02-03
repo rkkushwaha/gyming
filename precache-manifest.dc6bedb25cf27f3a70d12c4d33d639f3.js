c1cee5ebf76eabd60a845f348ec15298d7b16968
Raushan <kr93554.rk@gmail.com>
2022-02-03 09:46:34 +0530

    Updates

---
+++ precache-manifest.dc6bedb25cf27f3a70d12c4d33d639f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33420302331dcdbfe35c4467e1399a9e",
+    "url": "/gyming/index.html"
+  },
+  {
+    "revision": "0710fd307b7dfa91a22c",
+    "url": "/gyming/static/css/main.3f2a3ae3.chunk.css"
+  },
+  {
+    "revision": "6b1b56ae2f1359bfcb74",
+    "url": "/gyming/static/js/2.8797266e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/gyming/static/js/2.8797266e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0710fd307b7dfa91a22c",
+    "url": "/gyming/static/js/main.1ee74d3f.chunk.js"
+  },
+  {
+    "revision": "53cf10d43f88c04ee7c5",
+    "url": "/gyming/static/js/runtime-main.470ac867.js"
+  }
+]);
