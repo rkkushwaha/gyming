b9f77b0a8214b552d0124b7d5da4b228151c3796
Raushan <kr93554.rk@gmail.com>
2022-02-03 12:08:46 +0530

    master

---
+++ build/precache-manifest.b13818af7d66da5124b3062c10dbb5b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ab3a91638191f27f40c053f321096e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf3eefa6923c354f1282",
+    "url": "/static/css/main.1008363b.chunk.css"
+  },
+  {
+    "revision": "6b1b56ae2f1359bfcb74",
+    "url": "/static/js/2.8797266e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8797266e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf3eefa6923c354f1282",
+    "url": "/static/js/main.1ee74d3f.chunk.js"
+  },
+  {
+    "revision": "15bb9ce42bb839632136",
+    "url": "/static/js/runtime-main.b6ad969b.js"
+  }
+]);
